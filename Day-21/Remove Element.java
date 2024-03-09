8306045adb598013a64a2cd27d5f8e3ea09fa90c
GUNDU UDAY <udaygundu17@gmail.com>
2024-03-09 19:49:50 +0530

    Create Remove Element.java

---
+++ Day-21/Remove Element.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int k = 0;
+        for(int i = 0; i<nums.length; i++){
+            if(nums[i] != val){
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        // for(int i = 0; i<k; i++){
+        //     if(nums[i] != val){
+                
+        //     }
+        // }
+        return k;
+    }
+}
