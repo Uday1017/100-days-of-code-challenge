b6662eaa6a2152fda88806495f4d62c19c8c9dc7
GUNDU UDAY <udaygundu17@gmail.com>
2024-03-20 22:08:27 +0530

    Create 𝐀𝐂 𝐏𝐥𝐞𝐚𝐬𝐞.java

---
+++ Day-32/𝐀𝐂 𝐏𝐥𝐞𝐚𝐬𝐞.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		if(T > 30){
+		    System.out.print("YES");
+		}else{
+		    System.out.print("NO");
+		}
+
+	}
+}
