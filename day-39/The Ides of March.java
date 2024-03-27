56efce385fcfedf0b475ceddec5868141b9398bb
GUNDU UDAY <udaygundu17@gmail.com>
2024-03-27 21:41:34 +0530

    Create The Ides of March.java

---
+++ day-39/The Ides of March.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		if(N>=15){
+		    System.out.println("Yes");
+		}else{
+		    System.out.println("No");
+		}
+		
+
+	}
+}
