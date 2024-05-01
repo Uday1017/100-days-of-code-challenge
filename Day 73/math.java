534e11ec139106ed4e61eb0cc1e045dc2cdc4f4d
GUNDU UDAY <udaygundu17@gmail.com>
2024-05-01 23:38:38 +0530

    Create math.java

---
+++ Day 73/math.java
@@ -0,0 +1,14 @@
+public class maths {
+    public static void main(String[] args){
+        //Maths
+        //5,6
+        System.out.println(Math.max(5,6));
+        System.out.println(Math.min(5,6));
+        //random
+        System.out.println((int)Math.random());
+        //random values will generate b/w 0 to 1 so whenever we keep int ot will give 0
+        System.out.println((int)(Math.random()*100));
+
+    }
+    
+}
