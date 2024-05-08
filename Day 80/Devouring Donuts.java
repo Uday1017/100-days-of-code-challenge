3df344643107d372d25f9d0a078ea840249bc9ed
GUNDU UDAY <udaygundu17@gmail.com>
2024-05-08 23:38:51 +0530

    Create Devouring Donuts.java

---
+++ Day 80/Devouring Donuts.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int X = sc.nextInt();
+		int Y = sc.nextInt();
+		System.out.println(X*Y);
+
+	}
+}
